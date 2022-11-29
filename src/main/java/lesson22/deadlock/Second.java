da73513255eaec31fff5df9616d04b4111ebc27a
Julia Litvonova <julia.lit1978@gmail.com>
2022-11-29 09:21:57 +0200

    Урок 22. Стандартные способы поддержки многопоточности. Самостоятельная проработка кода урока.

---
+++ src/main/java/lesson22/deadlock/Second.java
@@ -0,0 +1,21 @@
+package lesson22.deadlock;
+
+public class Second {
+    private First first;
+
+    public First getFirst() {
+        return first;
+    }
+
+    public void setFirst(final First first) {
+        this.first = first;
+    }
+
+    public synchronized String getString() {
+        return "Hello from second class";
+    }
+
+    public synchronized String getStringFromFirstClass() {
+        return first.getString();
+    }
+}
